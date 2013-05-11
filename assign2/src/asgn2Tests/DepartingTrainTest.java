40ae3ba8af6dbcb761fdfdd62bb1fb66a759022f
Phurpa <phurpawangchuk@hotmail.com>
2013-05-11 17:14:56 +1000

    Initial version of the departing train unit tests implementation.

---
+++ assign2/src/asgn2Tests/DepartingTrainTest.java
@@ -0,0 +1,19 @@
+package asgn2Tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.Assert.*;
+
+import asgn2Train.DepartingTrain;
+
+public class DepartingTrainTest {
+	/**
+	 * Create a new departing train and do not add a locomotive, then
+	 * test if train can move without locomotive. Expected false.
+	 */
+	@Test
+	public void testTrainCannotMoveWithoutLocomotive(){
+		DepartingTrain departingTrain = new DepartingTrain();
+		Assert.assertFalse("train cannot move without locomotive", departingTrain.trainCanMove());
+	}
+}
